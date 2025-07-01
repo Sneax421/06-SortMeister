18dc2e1498bb11688674d7c85464db73d8b52184
Sneax421 <curilov.german@web.de>
2025-07-01 15:26:33 +0200

    added extendedItemDTO for item page

--- src/main/java/de/upteams/sortmeister/service/ItemService.java
+++ src/main/java/de/upteams/sortmeister/service/ItemService.java
@@ -1,6 +1,7 @@
 package de.upteams.sortmeister.service;
 
 import de.upteams.sortmeister.dto.ContainerDto;
+import de.upteams.sortmeister.dto.ExtendedItemDto;
 import de.upteams.sortmeister.dto.ItemResult;
 import de.upteams.sortmeister.model.Container;
 import de.upteams.sortmeister.model.Item;
@@ -28,6 +29,12 @@ public class ItemService {
 
     public Optional<Item> getById(Long id) {
         return repository.findById(id);
+    }
+
+    public ExtendedItemDto getExtededItemById(Long id) {
+        Item item = repository.findById(id).orElseThrow();
+        ContainerDto container = containerService.getById(item.getContainerId()).map(c-> new ContainerDto(c.getId(), c.getName(), c.getColor(), c.getDescription())).orElseThrow();
+        return new ExtendedItemDto(item.getId(), item.getName(), container);
     }
 
     public List<Item> search(String name) {
